6837b65730b901247e4301d3bc9706078365d8c1
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-05-11 19:19:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |index, dwarves|
+    index.to_i
     puts "#{index + 1}. #{dwarves}"
   end
 end
